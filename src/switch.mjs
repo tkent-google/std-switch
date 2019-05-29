41912f957c16f5a0b07fe793f8374780c066a6d3
TAMURA, Kent <tkent@chromium.org>
2019-05-29 14:15:06 +0900

    Implement theme swithing

--- src/switch.mjs
+++ src/switch.mjs
@@ -56,20 +56,23 @@ function installStringReflection(obj, attrName, propName = attrName) {
   });
 }
 
-const APP_MATCH_PLATFORM = 'match-platform';
+const PROP_CONTROL_THEME = '--std-control-theme';
+const THEME_DEFAULT = 'default';
+const THEME_MATCH_PLATFORM = 'match-platform';
+const THEME_MATERIAL = 'material';
+const THEME_COCOA = 'cocoa';
+const THEME_FLUENT = 'fluent';
 
-function isMaterial(app) {
-  return app == 'material' || app == APP_MATCH_PLATFORM && navigator.platform == 'Android';
+function effectiveTheme(theme) {
+  let platform = navigator.platform;
+  if (theme == THEME_MATERIAL || theme == THEME_MATCH_PLATFORM && platform == 'Android')
+    return THEME_MATERIAL;
+  if (theme == THEME_COCOA || theme == THEME_MATCH_PLATFORM && (platform.startsWith('iP') || platform.startsWith('Mac')))
+    return THEME_COCOA;
+  if (theme == THEME_FLUENT || theme == THEME_MATCH_PLATFORM && platform.startsWith('Win'))
+    return THEME_FLUENT;
+  return THEME_DEFAULT;
 }
-
-function isCocoa(app) {
-  return app == 'cocoa' || app == APP_MATCH_PLATFORM && (navigator.platform.startsWith('iP') || navigator.platform.startsWith('Mac'));
-}
-
-function isFluent(app) {
-  return app == 'fluent' || app == APP_MATCH_PLATFORM && navigator.platform.startsWith('Win');
-}
-
 
 const kMeterStyle = `
 :host {
@@ -85,7 +88,7 @@ const kMeterStyle = `
 
 .bar {
   display: inline-block;
-  background: var(--std-meter-value-background, lime);
+  background: var(--std-meter-value-background, red);
   block-size: 100%;
   inline-size: 0%;
   border-radius: var(--std-meter-value-radius);
@@ -102,26 +105,48 @@ const kSwitchStyle = `
   inline-size: 54px;
   block-size: 26px;
   box-sizing: border-box;
+  vertical-align: bottom;
+  user-select: none;
 
   /* Internal variables based on exposed variables */
+  --i-color-on: var(--std-switch-color-on, #0077ff);
+  --i-track-height: var(--std-switch-track-height, 100%);
   --i-track-border: var(--std-switch-track-border, 2px solid #dddddd);
   --i-track-border-on: var(--std-switch-track-border-on, 2px solid var(--i-color-on));
+  --i-track-border-on-active: var(--std-switch-track-border-on-active, 2px solid #77bbff);
   --i-track-radius: var(--std-switch-track-radius, 13px);
   --i-track-padding: var(--std-switch-track--padding, 0px);
   --i-track-inner-radius: var(--std-switch-track-inner-radius, 11px);
   --i-track-background: var(--std-switch-track-background, transparent);
+  --i-track-background-active: var(--std-switch-track-background-active, var(--std-switch-track-background, #dddddd));
   --i-track-background-on: var(--std-switch-track-background-on, var(--i-color-on));
+  --i-track-background-on-active: var(--std-switch-track-background-on-active, var(--std-switch-track-background-on, #77bbff));
   --i-track-shadow: var(--std-switch-track-shadow, 0 0 0 1px #f8f8f8);
   --i-track-shadow-focus: var(--std-switch-track-shadow-focus, 0 0 0 2px #f8f8f8);
   --i-track-shadow-on: var(--std-switch-track-shadow-on);
   --i-track-shadow-on-focus: var(--std-switch-track-shadow-on-focus, 0 0 0 2px #dddddd);
-  --i-color-on: var(--std-switch-color-on, #0077ff);
+  --i-track-shadow-on-active: var(--std-switch-track-shadow-on-active, 0 0 0 2px #f8f8f8);
+
+  --i-thumb-width: var(--std-switch-thumb-width, 22px);
+  --i-thumb-width-hover: var(--std-switch-thumb-width-hover, 26px);
+  --i-thumb-height: var(--std-switch-thumb-height, 22px);
+  --i-thumb-radius: var(--std-switch-thumb-radius, calc(var(--i-thumb-height) / 2));
+  --i-thumb-border: var(--std-switch-thumb-border, 1px solid black);
+  --i-thumb-border-focus: var(--std-switch-thumb-border-focus, 2px solid black);
+  --i-thumb-border-on: var(--std-switch-thumb-border-on, 1px solid #0077ff);
+  --i-thumb-border-on-focus: var(--std-switch-thumb-border-on-focus, 2px solid #0077ff);
+  --i-thumb-background: var(--std-switch-thumb-background, white);
+  --i-thumb-background-on: var(--std-switch-thumb-background-on, var(--std-switch-thumb-background, white));
+  --i-thumb-margin-start: var(--std-switch-thumb-margin-start, 2px);
+  --i-thumb-margin-end: var(--std-switch-thumb-margin-end, 2px);
+  --i-thumb-shadow: var(--std-switch-thumb-shadow);
+
 
   --std-meter-border: var(--i-track-border);
   --std-meter-radius: var(--i-track-radius);
   --std-meter-padding: var(--i-track-padding);
   --std-meter-value-radius: var(--i-track-inner-radius);
-  --std-meter-value-background: var(--i-track-background);
+  --std-meter-value-background: var(--i-track-background-on);
 }
 
 :host(:disabled) {
@@ -135,7 +160,6 @@ const kSwitchStyle = `
 :host([on]) internal-meter  {
   border: var(--i-track-border-on);
   box-shadow: var(--i-track-shadow-on);
-  --std-meter-value-background: var(--i-track-background-on);
 }
 
 :host(:focus) internal-meter {
@@ -148,19 +172,20 @@ const kSwitchStyle = `
 /* We can't use :enabled until all major browsers support FACE. */
 
 :host(:not(:disabled):active) internal-meter {
-  background-color: #dddddd;
+  background: var(--i-track-background-active);
 }
 :host([on]:not(:disabled):active) internal-meter {
-  --std-meter-value-background: #77bbff;
-  border-color: #77bbff;
-  box-shadow: 0 0 0 2px #f8f8f8;
+  --std-meter-value-background: var(--i-track-background-on-active);
+  border: var(--i-track-border-on-active);
+  box-shadow: var(--i-track-shadow-on-active);
 }
 
 internal-meter {
+  background: var(--i-track-background);
   box-shadow: var(--i-track-shadow);
   inline-size: 100%;
-  block-size: var(--std-switch-track-height, 100%);
-  transition: all linear 0.1s;
+  block-size: var(--i-track-height);
+  transition: all linear 0.1s; /* TODO(customizable) */
 }
 
 .container {
@@ -172,44 +197,86 @@ internal-meter {
 }
 
 .thumb {
-  --thumb-width: var(--std-switch-thumb-width, 22px);
-  --thumb-width-hover: var(--std-switch-thumb-width, 26px);
-  --thumb-height: 22px;
-  --thumb-radius: calc(var(--thumb-height) / 2);
-  --thumb-right-margin: var(--std-switch-thumb-right-margin, 2px);
-
   position: absolute !important;
   display: inline-block;
-  border: var(--std-switch-thumb-border, 1px solid black);
-  border-radius: var(--thumb-radius);
-  inline-size: var(--thumb-width);
-  block-size: var(--thumb-height);
-  background: white;
+  border: var(--i-thumb-border);
+  border-radius: var(--i-thumb-radius);
+  inline-size: var(--i-thumb-width);
+  block-size: var(--i-thumb-height);
+  background: var(--i-thumb-background);
   box-sizing: border-box;
-  inset-inline-start: var(--std-switch-thumb-left-margin, 2px);
-  transition: all linear 0.1s;
+  inset-inline-start: var(--i-thumb-margin-start);
+  transition: all linear 0.1s; /* TODO(customizable) */
+  box-shadow: var(--i-thumb-shadow);
 }
 
 :host(:not(:disabled):hover) .thumb {
-  inline-size: var(--thumb-width-hover);
+  inline-size: var(--i-thumb-width-hover);
 }
 
 :host([on]) .thumb {
-  border: var(--std-switch-thumb-border-on, 1px solid #0077ff);
-  inset-inline-start: calc(100% - var(--thumb-width) - var(--thumb-right-margin));
+  background: var(--i-thumb-background-on);
+  border: var(--i-thumb-border-on);
+  inset-inline-start: calc(100% - var(--i-thumb-width) - var(--i-thumb-margin-end));
 }
 
 :host(:focus) .thumb {
-  border-width: 2px;
+  border: var(--i-thumb-border-focus);
+}
+
+:host([on]:focus) .thumb {
+  border: var(--i-thumb-border-on-focus);
 }
 
 :host([on]:not(:disabled):hover) .thumb {
-  inset-inline-start: calc(100% - var(--thumb-width-hover) - var(--thumb-right-margin));
+  inset-inline-start: calc(100% - var(--i-thumb-width-hover) - var(--i-thumb-margin-end));
 }
 `;
 
-const kSwitchStyleMaterial = ``;
-const kSwitchStyleCocoa = ``;
+const kSwitchStyleMaterial = `
+:host {
+  inline-size: 36px;
+  block-size: 20px;
+  --std-switch-track-height: 14px;
+  --std-switch-track-background: rgba(0,0,0,0.4);
+  --std-switch-track-background-on: rgba(63,81,181,0.5);
+  --std-switch-track-border: none;
+  --std-switch-track-border-on: none;
+  --std-switch-track-border-on-active: none;
+  --std-switch-thumb-margin-start: 0px;
+  --std-switch-thumb-margin-end: 0px;
+  --std-switch-thumb-width: 20px;
+  --std-switch-thumb-width-hover: 20px;
+  --std-switch-thumb-height: 20px;
+  --std-switch-thumb-shadow: 0 1px 5px 0 rgba(0,0,0,0.6);
+  --std-switch-thumb-border: none;
+  --std-switch-thumb-border-focus: none;
+  --std-switch-thumb-border-on: none;
+  --std-switch-thumb-border-on-focus: none;
+  --std-switch-thumb-background-on: rgb(63,81,181);
+}
+
+`;
+
+const kSwitchStyleCocoa = `
+:host {
+  inline-size: 51px;
+  block-size: 31px;
+  --std-switch-track-radius: 15px;
+  --std-switch-track-background-on: #4ad963;
+  --std-switch-track-border: 1px solid lightgray;
+  --std-switch-track-border-on: 1px solid #4ad963;
+  --std-switch-thumb-height: 29px;
+  --std-switch-thumb-width: 29px;
+  --std-switch-thumb-width-hover: 29px;
+  --std-switch-thumb-border: none;
+  --std-switch-thumb-border-on: none;
+  --std-switch-thumb-margin-start: 1px;
+  --std-switch-thumb-margin-end: 1px;
+  --std-switch-thumb-shadow: 0px 3px 4px 1px rgba(0,0,0,0.2);
+}
+`;
+
 const kSwitchStyleFluent = ``;
 
 
@@ -235,10 +302,7 @@ class InternalMeterElement extends HTMLElement {
   constructor() {
     super();
     this._value = 0;
-  }
-
-  connectedCallback() {
-    this.maybeInitialize();
+    this._initializeDOM();
   }
 
   /**
@@ -249,12 +313,10 @@ class InternalMeterElement extends HTMLElement {
       throw new RangeError();
     let oldValue = this._value;
     this._value = i;
-    this.maybeInitialize();
-    //this._barElement.animate({width: [oldValue + '%', i + '%']}, 300);
     this._barElement.style.inlineSize = i + '%';
   }
 
-  maybeInitialize() {
+  _initializeDOM() {
     if (this._barElement)
       return;
     let doc = this.ownerDocument;
@@ -289,6 +351,7 @@ export class StdSwitchElement extends HTMLElement {
     installBoolReflection(this, 'disabled');
     installBoolReflection(this, 'on');
     installStringReflection(this, 'name');
+    this.addEventListener('click', this._onClick.bind(this));
   }
 
   get form() { return this._internals.form; }
@@ -297,26 +360,25 @@ export class StdSwitchElement extends HTMLElement {
   connectedCallback() {
     if (!this.isConnected)
       return;
-    let appearance = getComputedStyle(this).getPropertyValue('--std-control-appearance');
+    let theme = effectiveTheme(getComputedStyle(this).getPropertyValue(PROP_CONTROL_THEME).trim());
     this._initializeDOM();
-    this.addEventListener('click', this._onClick.bind(this));
     if (switchSheet) {
       let sheets = [switchSheet];
-      if (isMaterial(appearance)) {
+      if (theme == THEME_MATERIAL) {
         sheets.push(switchSheetMaterial);
-      } else if (isCocoa(appearance)) {
+      } else if (theme == THEME_COCOA) {
         sheets.push(switchSheetCocoa);
-      } else if (isFluent(appearance)) {
+      } else if (theme == THEME_FLUENT) {
         sheets.push(switchSheetFluent);
       }
       this._root.adoptedStyleSheets = sheets;
     } else {
       let styleText = kSwitchStyle;
-      if (isMaterial(appearance)) {
+      if (theme == THEME_MATERIAL) {
         styleText += kSwitchStyleMaterial;
-      } else if (isCocoa(appearance)) {
+      } else if (theme == THEME_COCOA) {
         styleText += kSwitchStyleCocoa;
-      } else if (isFluent(appearance)) {
+      } else if (theme == THEME_FLUENT) {
         styleText += kSwitchStyleFluent;
       }
       this._root.querySelector('style').textContent = styleText;
@@ -334,6 +396,7 @@ export class StdSwitchElement extends HTMLElement {
     container.className = 'container';
     this._root.appendChild(container);
     this._trackElement = container.appendChild(doc.createElement('internal-meter'));
+    this._trackElement.value = this.on ? 100 : 0;
     this._thumbElement = container.appendChild(doc.createElement('span'));
     this._thumbElement.className = 'thumb';
   }
